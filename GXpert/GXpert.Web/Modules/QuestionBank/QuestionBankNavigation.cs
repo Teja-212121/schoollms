65ee954e507aa195fb855bdf4a5b671b4c3e5c71
Pallavi Patil <pp036539@gmail.com>
2024-04-01 12:25:11 +0530

    Update question bank

--- GXpert/GXpert.Web/Modules/QuestionBank/QuestionBankNavigation.cs
+++ GXpert/GXpert.Web/Modules/QuestionBank/QuestionBankNavigation.cs
@@ -3,5 +3,5 @@ using MyPages = GXpert.QuestionBank.Pages;
 
 [assembly: NavigationMenu(7000, "QuestionBank", icon: "fa fa-question")]
 [assembly: NavigationLink(7001, "QuestionBank/Question", typeof(MyPages.QuestionPage), icon: "fa-circle-o")]
-//[assembly: NavigationLink(7003, "QuestionBank/Common Data", typeof(MyPages.CommonDataPage), icon: "fa-circle-o")]
-[assembly: NavigationLink(7003, "QuestionBank/Question Course", typeof(MyPages.QuestionCoursePage), icon: "fa-circle-o")]
+[assembly: NavigationLink(7003, "QuestionBank/Common Data", typeof(MyPages.CommonDataPage), icon: "fa-circle-o")]
+[assembly: NavigationLink(7004, "QuestionBank/Question Course", typeof(MyPages.QuestionCoursePage), icon: "fa-circle-o")]
